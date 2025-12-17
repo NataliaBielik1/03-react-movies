0cc0bc106b27e68938de927f2655853d090ce679
Nataliia Bielik <natalia.belic2607@gmail.com>
2025-12-16 22:40:46 -0500

    fixed mistake

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,19 +1,19 @@
 import axios from "axios";
 import type { Movie } from "../types/movie";
 
-interface TMDBRespons {
+interface TMDBResponsProps {
   results: Movie[];
   page: number;
   total_pages: number;
 }
 
 export const fetchMovies = async (query: string): Promise<Movie[]> => {
-  const response = await axios.get<TMDBRespons>(
+  const response = await axios.get<TMDBResponsProps>(
     "https://api.themoviedb.org/3/search/movie",
     {
       params: { query },
       headers: { Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}` },
     }
-    );
+  );
     return response.data.results
 };
